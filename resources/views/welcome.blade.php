0e64d00c3df3ebb229a919416837d32b0a3b11da
Matt Casanova <lazersquad@gmail.com>
2026-02-03 21:15:22 -0800

    End Ep 5 Passing Data to Views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Welcome">
-    <h1>Hello World</h1>
+    <h1>{{ $greeting }}, {{ $person }}!</h1>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'greeting' => 'Hello',
+        'person' => request('person', 'Guest'),
+    ]);
 });
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
