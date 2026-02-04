b71c0cd67bba6e033aa983c995f1bda03553ce10
Matt Casanova <lazersquad@gmail.com>
2026-02-03 23:10:28 -0800

    End Ep 8, Database and Eloquent

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -16,12 +16,12 @@
         </div>
     </form>
 
-    @if (count($ideas))
+    @if ($ideas->isNotEmpty())
         <h2 class="mb-4 text-lg font-semibold text-white">Your ideas</h2>
         @foreach ($ideas as $idea)
             <div
                 class="mb-4 rounded-md bg-white/5 px-3 py-2 text-white outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500">
-                {{ $idea }}
+                {{ $idea->description }}
             </div>
         @endforeach
     @endif
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,31 @@
 <?php
 
+use App\Models\Idea;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $ideas = session('ideas', []);
+    $ideas = Idea::query()->when(request('state'), function ($query, $state) {
+        $query->where('state', $state);
+    })->get();
+
     return view('ideas', [
-        'ideas' => $ideas
+        'ideas' => $ideas,
     ]);
 });
 
 Route::post('/ideas', function () {
-    $idea = request('idea');
-    if (!empty($idea)) {
-        session()->push('ideas', $idea);
-    }
+    Idea::create([
+        'description' => request('idea'),
+        'state' => 'pending',
+    ]);
 
     return redirect('/');
 });
 
 // temp delete route
 Route::get('/delete-ideas', function () {
-    session()->forget('ideas');
+    // delete all ideas
+    Idea::query()->delete();
+
     return redirect('/');
 });
