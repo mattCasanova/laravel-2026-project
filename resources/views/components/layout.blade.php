bd9a666d7d50700b66238bf61a24321d69d385e8
Matt Casanova <lazersquad@gmail.com>
2026-02-21 16:33:30 -0800

    End Ep 19, vite/tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,23 +2,22 @@
     'title' => 'My Website',
 ])
 
-<!doctype html>
-<html lang="en" data-theme="night">
+    <!doctype html>
+<html lang="en" data-theme="synthwave">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
-<body class="">
-    <x-nav />
-    <main class="mx-auto mt-6 max-w-3xl">
-        {{ $slot }}
-    </main>
+<body class="text-primary">
+<x-nav/>
+<main class="mx-auto mt-6 max-w-3xl">
+    {{ $slot }}
+</main>
 </body>
 
 </html>
